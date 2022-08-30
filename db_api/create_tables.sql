c61ebd864baa5c47413559f250176f12c8223cd2
Daniil Ryzhov <53818324+bl4ckm45k@users.noreply.github.com>
2022-08-30 12:19:01 +0300

    Удалено создание неиспользуемой таблицы

--- db_api/create_tables.sql
+++ db_api/create_tables.sql
@@ -1,15 +1,3 @@
-create table tg_users
-(
-    id      bigserial PRIMARY KEY,
-    user_id BIGINT NOT NULL UNIQUE,
-    active  boolean DEFAULT FALSE,
-    server_id INTEGER NULL,
-    key_id  INTEGER NULL DEFAULT NULL
-);
-
-alter table tg_users
-    owner to postgres;
-
 create table vpn_servers
 (
     id      bigserial PRIMARY KEY,
