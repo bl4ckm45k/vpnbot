78e573cbede4968ac308d15c2fef4ca1bb58a902
bl4ckm45k <nonpowa@gmail.com>
2024-05-13 13:42:31 +0300

    delete db usage

--- update.sh
+++ update.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Останавливаем все контейнеры с образом custom_vpn_bot
+# Останавливаем все контейнеры с образом free_vpn_bot
 docker ps -a | grep free_vpn_bot | awk '{print $1}' | xargs docker stop
-# Удаляем все контейнеры с образом custom_vpn_bot
+# Удаляем все контейнеры с образом free_vpn_bot
 docker ps -a | grep free_vpn_bot | awk '{print $1}' | xargs docker rm
-# Удаляем образ custom_vpn_bot
+# Удаляем образ free_vpn_bot
 docker rmi free_vpn_bot
-# Создаем новый образ custom_vpn_bot
+# Создаем новый образ free_vpn_bot
 docker build -t free_vpn_bot .
-# docker ps -a | grep gozargah/marzban:latest | awk '{print $1}' | xargs docker stop
-# Удаляем все контейнеры с образом custom_vpn_bot
-# docker ps -a | grep gozargah/marzban:latest | awk '{print $1}' | xargs docker rm
-# Выполним скрипт install.sh
-./install.sh
+# Останавливаем и удаляем контейнер marzban
+docker stop free_vpn_bot_marzban
+docker rm free_vpn_bot_marzban
+# Поднимаем контейнеры
+docker compose up -d --remove-orphans
